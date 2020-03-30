ceec15f2109517cf0e3f2d86365c5c98fdd35e92
fritzen <finn-blumenstock@web.de>
2020-03-30 19:40:05 +0200

    changed project type to .netstandard

---
+++ Lazagne.Net.Console/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lazagne.Net.Run
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<ApplicationInfo> credentials = ModuleHandler.GetCredentials();
+
+            foreach (ApplicationInfo application in credentials)
+            {
+                Console.WriteLine(application);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
